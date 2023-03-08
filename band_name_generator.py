5de7134679c271f8cc9b2d41c8bc5f0a1301ac9b
Kunwar Singh Gujral <csgujrals@gmail.com>
2023-03-08 03:09:52 -0500

    Add files via upload

---
+++ band_name_generator.py
@@ -0,0 +1,14 @@
+#1. Create a greeting fo your program
+print("Hello, Welcome to the program by Kunwar Singh Gujral")
+
+#2. Ask the user for the city that they grew up in
+city_name = input("Enter the name of the city where you grew up in\n")
+
+#3. Ask the user for the name of a pet
+pet_name = input("Enter the name of your pet\n")
+
+#4. Combine the name of their city and pet and them their band name.
+band_name = city_name+" "+pet_name
+print("Your band name is: "+band_name)
+
+#5. Make sure that the cursor shows on a new line
