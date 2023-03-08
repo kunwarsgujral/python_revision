f7afa1a9c2d58c13a1c4f9b29acb37feca03fbf4
Kunwar Singh Gujral <csgujrals@gmail.com>
2023-03-08 04:14:39 -0500

    Add files via upload

---
+++ round_a_number_and_fstrings.py
@@ -0,0 +1,12 @@
+print(round(8/3,2))
+print(8 // 3 ) # floor division
+print(type(8/3))
+print(type(8//3))
+score = 0 
+score += 1 # short hand  for doing score = score + 1
+print("Score is "+str(score))
+print(f"Score is {score}") # this is f-string 
+
+height = 1.8
+isWinning = True 
+print(f"Your score is {score},your height is {height}, you are winning is {isWinning}" )
