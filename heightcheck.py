c51938bc3c1034085af7cd2cb717b0ef250d2f3e
Kunwar Singh Gujral <csgujrals@gmail.com>
2023-03-10 09:46:37 -0500

    Add files via upload

---
+++ heightcheck.py
@@ -0,0 +1,8 @@
+print("Welcome to Height Check")
+user_height = float(input("Enter your height in cm: "))
+if user_height > 120:
+	print("Welcome to the ride")
+else:
+	print("You are not allowed on the ride")
+
+ 
